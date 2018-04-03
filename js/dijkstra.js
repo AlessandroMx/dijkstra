8d93418187d0175cb311d6b4250ef8e75a0242fb
Alessandro <alessandromx@outlook.com>
2018-04-02 22:35:31 -0500

    Button settings addes... Need to be fix some iissues

--- js/dijkstra.js
+++ js/dijkstra.js
@@ -1,13 +1,12 @@
 window.addEventListener("load", function () {
     $('#button-play').click(function () {
-        
+
         // Remove previous canvas
         $('#canvas').remove();
         $('#canvas-container').append('<canvas id="canvas" width="640" height="420"></canvas>');
 
-        // Enable buttons
+        // Enable add node button
         $('#button-add-node').prop('disabled', false);
-        $('#button-rem-node').prop('disabled', false);
 
         // Start main logic
         main();
@@ -22,6 +21,7 @@ class Node {
         this.id = id;
         this.x = x;
         this.y = y;
+        // $('#button-rem-node').prop('disabled', false);
         this.ctx = ctx;
         this.neighbors = neighbors || null;
     }
@@ -44,7 +44,17 @@ class Node {
 
 }
 
+let modes = {
+    addNodes: true,
+    remNodes: false,
+    addRoutes: false,
+    remRoutes: false
+};
+
 let main = function () {
+
+    // Disabled buttons
+    disableButtons();
 
     // Instantiate canvas and context
     let canvas = document.querySelector('#canvas');
@@ -60,34 +70,114 @@ let main = function () {
     // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Set Nodes...
-    let nodeCnt = 1;
+    // Set Nodes and Routes...
+    let nodeCnt = 0;
     let nodeArray = [];
+    let routeCnt = 0;
+    let routeArray = [];
 
     // Be aware of window resize
-    window.addEventListener('resize', function(e) {
+    window.addEventListener('resize', function (e) {
         canvasLeft = getOffsetLeft(canvas);
         canvasTop = canvas.offsetTop;
     });
 
     // Be aware of clicks
     canvas.addEventListener('click', function (e) {
-        
+
         let clickX = e.pageX - canvasLeft;
         let clickY = e.pageY - canvasTop;
-        
-        let tmpNode = new Node(nodeCnt, clickX, clickY, ctx);
-        
-        tmpNode.draw();
-        nodeArray.push(tmpNode);
-        
-        nodeCnt += 1;
+
+        if (modes.addNodes) {
+
+            nodeCnt += 1;
+
+            let tmpNode = new Node(nodeCnt, clickX, clickY, ctx);
+
+            // tmpNode.draw();
+            nodeArray.push(tmpNode);
+
+        } else if (modes.remNodes) {
+            // PENSARRRR MAAAXSSS
+            for (let node in nodeArray) {
+                
+                let needToBeRemoved = false;
+
+                let tmpX = nodeArray[node].x;
+                let tmpY = nodeArray[node].y;
+
+                if (clickX <= tmpX + 15 || clickX >= tmpX - 15) {
+                    if (clickY <= tmpY + 15 || clickY >= tmpY - 15) {
+                        needToBeRemoved = true;
+                    }
+                }
+
+                if (needToBeRemoved) {
+                    nodeArray = nodeArray.splice(node, 1);
+                    nodeCnt -= 1;
+                }
+
+            }
+
+        }
+
+        // Enable required buttons
+        enableRequiredButtons(nodeCnt, routeCnt);
+
+        // Update Canvas
+        updateCanvas(ctx, nodeArray);
+
     });
+
+    // Check if any button has been clicked
+    checkClickedButton();
 
 }
 
+// Enable all necessary buttons according to the number of nodes and their routes
+let enableRequiredButtons = function (nodeCnt, routeCnt) {
+    if (nodeCnt > 0) $('#button-rem-node').prop('disabled', false);
+    if (nodeCnt > 1) $('#button-add-route').prop('disabled', false);
+    if (routeCnt > 0) $('#button-add-route').prop('disabled', false);
+}
+
+// Check which button has been clicked to set the mode
+let checkClickedButton = function () {
+    $('#container-buttons').find('button').click(function () {
+        setButtonsToSecondary();
+        $(this).toggleClass('uk-button-secondary');
+        $(this).toggleClass('uk-button-danger');
+        setMode($(this).val());
+    });
+}
+
+// Set all buttons' classes to 'uk-button-secondary'
+let setButtonsToSecondary = function () {
+    $('#container-buttons').find('button').each(function () {
+        $(this).removeClass('uk-button-danger');
+        $(this).removeClass('uk-button-secondary');
+        $(this).addClass('uk-button-secondary');
+    });
+}
+
+// Set the mode of the click according to the option clicked
+let setMode = function(mode) {
+    for (let mode in modes) {
+        modes[mode] = false;
+    }
+    modes[mode] = true;
+}
+
+// Update canvas
+let updateCanvas = function(ctx, nodeArray) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let node in nodeArray) {
+        nodeArray[node].draw();
+    }
+}
+
 // Util functions
-function getOffsetLeft(elem) {
+let getOffsetLeft = function (elem) {
     var offsetLeft = 0;
     do {
         if (!isNaN(elem.offsetLeft)) {
@@ -95,4 +185,10 @@ function getOffsetLeft(elem) {
         }
     } while (elem = elem.offsetParent);
     return offsetLeft;
+}
+
+let disableButtons = function () {
+    $('#button-rem-node').prop('disabled', true);
+    $('#button-add-route').prop('disabled', true);
+    $('#button-rem-route').prop('disabled', true);
 }
